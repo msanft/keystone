e3d6c8b898b60bbc4642d017a131c772c46845b7
Moritz Sanft <moritz.sanft@outlook.de>
2023-05-26 20:57:58 +0200

    swtpm setup script

---
+++ scripts/setup-swtpm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# check if swtpm path exists
+if [ ! -f /swtpm ]; then
+    echo "swtpm not found, cloning from remote"
+    git clone https://github.com/stefanberger/swtpm
+fi
+
+# build swtpm
+sudo apt-get install dh-autoreconf libssl-dev \
+     libtasn1-6-dev pkg-config libtpms-dev \
+     net-tools iproute2 libjson-glib-dev \
+     libgnutls28-dev expect gawk socat \
+     libseccomp-dev make -y
+
+cd swtpm
+
+./autogen.sh --with-openssl --prefix=/usr
+
+make -j4
+
+# tests
+make -j4 check
+
+sudo make install
