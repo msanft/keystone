d6ef12143d2297c10055baf2c9ce219cee7cc730
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2023-06-28 16:32:57 +0200

    rename colliding methods

--- bootrom/bootloader.c
+++ bootrom/bootloader.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 
 #define ED25519_NO_SEED 1
-#include "sha3/sha3.h"
+#include "ed25519/ed25519.h"
 /* Adopted from https://github.com/orlp/ed25519
   provides:
   - void ed25519_create_keypair(t_pubkey *public_key, t_privkey *private_key, t_seed *seed);
@@ -12,18 +12,18 @@
                       t_privkey *private_key);
 */
 
-#include "ed25519/ed25519.h"
+#include "sha3/sha3.h"
 /* adopted from
   provides:
-  - int sha3_init(sha3_context * md);
-  - int sha3_update(sha3_context * md, const unsigned char *in, size_t inlen);
-  - int sha3_final(sha3_context * md, unsigned char *out);
+  - int keystone_sha3_init(sha3_context * md);
+  - int keystone_sha3_update(sha3_context * md, const unsigned char *in, size_t inlen);
+  - int keystone_sha3_final(sha3_context * md, unsigned char *out);
   types: sha3_context
 */
 
 #include "string.h"
 /*
-  provides memcpy, memset
+  provides keystone_memcpy, keystone_memset
 */
 
 #include "jent/jitterentropy.h"
@@ -88,28 +88,28 @@ void bootloader()
   // ed25519_create_keypair(sanctum_dev_public_key, sanctum_dev_secret_key, scratchpad);
 
   // Measure SM
-  sha3_init(&hash_ctx, 64);
-  sha3_update(&hash_ctx, (void *)DRAM_BASE, sanctum_sm_size);
-  sha3_final(sanctum_sm_hash, &hash_ctx);
+  keystone_sha3_init(&hash_ctx, 64);
+  keystone_sha3_update(&hash_ctx, (void *)DRAM_BASE, sanctum_sm_size);
+  keystone_sha3_final(sanctum_sm_hash, &hash_ctx);
 
   // Combine SK_D and H_SM via a hash
   // sm_key_seed <-- H(SK_D, H_SM), truncate to 32B
-  sha3_init(&hash_ctx, 64);
-  sha3_update(&hash_ctx, sanctum_dev_secret_key, sizeof(*sanctum_dev_secret_key));
-  sha3_update(&hash_ctx, sanctum_sm_hash, sizeof(*sanctum_sm_hash));
-  sha3_final(scratchpad, &hash_ctx);
+  keystone_sha3_init(&hash_ctx, 64);
+  keystone_sha3_update(&hash_ctx, sanctum_dev_secret_key, sizeof(*sanctum_dev_secret_key));
+  keystone_sha3_update(&hash_ctx, sanctum_sm_hash, sizeof(*sanctum_sm_hash));
+  keystone_sha3_final(scratchpad, &hash_ctx);
   // Derive {SK_D, PK_D} (device keys) from the first 32 B of the hash (NIST endorses SHA512 truncation as safe)
   ed25519_create_keypair(sanctum_sm_public_key, sanctum_sm_secret_key, scratchpad);
 
   // Endorse the SM
-  memcpy(scratchpad, sanctum_sm_hash, 64);
-  memcpy(scratchpad + 64, sanctum_sm_public_key, 32);
+  keystone_memcpy(scratchpad, sanctum_sm_hash, 64);
+  keystone_memcpy(scratchpad + 64, sanctum_sm_public_key, 32);
   // Sign (H_SM, PK_SM) with SK_D
   ed25519_sign(sanctum_sm_signature, scratchpad, 64 + 32, sanctum_dev_public_key, sanctum_dev_secret_key);
 
   // Clean up
   // Erase SK_D
-  memset((void *)sanctum_dev_secret_key, 0, sizeof(*sanctum_dev_secret_key));
+  keystone_memset((void *)sanctum_dev_secret_key, 0, sizeof(*sanctum_dev_secret_key));
 
   // caller will clean core state and memory (including the stack), and boot.
   return;
